a4d8dd38a3dfc33d694acd42e380e743d43c492b
JesusCangalaya1 <cperaltapenaloza@gmail.com>
2025-01-24 00:22:27 -0500

    feature: inserts Moneda

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO moneda (codigo, nombre) VALUES ('PEN', 'Sol Peruano');
+INSERT INTO moneda (codigo, nombre) VALUES ('USD', 'Dólar Estadounidense');
+INSERT INTO moneda (codigo, nombre) VALUES ('EUR', 'Euro');
